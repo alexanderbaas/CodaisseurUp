e68c11aaa600f501cdc86ca738543078a37958f5
Alexander Baas <ajmbaas@outlook.com>
2017-10-11 17:58:07 +0200

    one to one and added profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ApplicationRecord
   has_many :events, dependent: :destroy
-
+  has_one :profile
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+            def has_profile?
+             profile.present? && profile.persisted?
+            end
 
+            def full_name
+             profile.full_name
+            end
 
 end
