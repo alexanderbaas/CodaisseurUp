34f6c1f6fbed0550eaf35639177545904b819d05
Alexander Baas <ajmbaas@outlook.com>
2017-10-11 18:37:57 +0200

    add gems and first rspec test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,4 @@
-#
-#
-# # Theme.destroy_all
-# User.destroy_all
-#
-#
-# meneer = User.create!({email: "email@email.com", password: "abcd12334" })
-# kerel = User.create!({email: "emal@email.com", password: "abcd12334" })
-# menneke = User.create!({email: "mail@email.com", password: "abcd12334" })
-# gast = User.create!({email: "emai@email.com", password: "abcd12334" })
-# knul = User.create!({email: "eail@email.com", password: "abcd12334" })
-# guy = User.create!({email: "emdail@email.com", password: "abcd12334" })
-#
-# event1 = Event.create!({name: "Music4More",
-#                      description: "Festival!!!",
-#                      location: "Utreg!",
-#                      price: 25.00,
-#                      capacity: 5000,
-#                      includes_food: true,
-#                      includes_drinks: true,
-#                      starts_at: DateTime.parse("10/12/2017 17:00"),
-#                      ends_at: DateTime.parse("11/12/2017 01:00"),
-#                      active: true,
-#                      user: meneer})
+
 Theme.delete_all
 Event.delete_all
 User.delete_all
