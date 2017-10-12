0133bc93d6dcd9f12a9ab37ec30af6d5947fa669
Alexander Baas <ajmbaas@outlook.com>
2017-10-12 10:28:57 +0200

    Add CarrierWave and generate image uploader, css form container

--- spec/views/pages/home.html.erb_spec.rb
+++ spec/views/pages/home.html.erb_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "pages/home.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  
+
+
+
+
+
+
 end
